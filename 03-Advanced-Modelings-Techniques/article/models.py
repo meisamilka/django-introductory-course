12c02c27259e07b1ae71569303cdf263d07cb66b
“meisamilka” <meisam.ilka@gmail.com>
2025-07-25 11:28:00 +0300

    Add UserProfile model to 'article' app with fields for national ID, first name, and last name; create corresponding migration.

--- 03-Advanced-Modelings-Techniques/article/models.py
+++ 03-Advanced-Modelings-Techniques/article/models.py
@@ -22,3 +22,8 @@ class Teacher(models.Model):
         return f"{self.name} {self.family}"
 
 
+class UserProfile(models.Model):
+    national_id = models.CharField(
+        max_length=10, unique=True, null=True, blank=True)
+    first_name = models.CharField(max_length=100, null=True, blank=True)
+    last_name = models.CharField(max_length=100, null=True, blank=True)
